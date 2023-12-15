e48c844a052e2b8c24f91aca455c7b7079c122f2
femmytedrey <femmytedrey@gmail.com>
2023-12-15 17:48:36 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return <h2>accordion project setup</h2>; j
 }
 
 export default App;
